783cfedcfc093cfe964b9c95cbdcbaff1fa69734
Rithunyaa <rithunyaa@gmail.com>
2026-06-22 21:54:05 -0400

    End of Day 4
    Have a basic prototype done; with animations but still looks pretty blurry

--- wind-turbine/scenes/bird.gd
+++ wind-turbine/scenes/bird.gd
@@ -1,24 +1,53 @@
 extends CharacterBody2D
 
-@export var gravity = 900
-@export var jump_force = -350
+@export var speed = 500
+@onready var sprite = $AnimatedSprite2D
+@onready var margin = Vector2(32, 32)
+@onready var screen_size = get_viewport_rect().size
 
-@onready var anim = $AnimatedSprite2D
+
+func _ready():
+	sprite.play("idle")
+	add_to_group("player")
 
 func _physics_process(delta):
-	# gravity
-	velocity.y += gravity * delta
+	var direction = Vector2.ZERO
 
-	# flap input
-	if Input.is_action_just_pressed("ui_accept"):
-		velocity.y = jump_force
-		anim.play("flap")
+	if Input.is_action_pressed("move_right"):
+		direction.x += 1
 
-	# auto animation fallback
-	if velocity.y > 0:
-		anim.play("fall")
-	else:
-		if not Input.is_action_just_pressed("ui_accept"):
-			anim.play("idle")
+	if Input.is_action_pressed("move_left"):
+		direction.x -= 1
 
+	if Input.is_action_pressed("move_down"):
+		direction.y += 1
+
+	if Input.is_action_pressed("move_up"):
+		direction.y -= 1
+
+	velocity = direction.normalized() * speed
 	move_and_slide()
+
+	# Choose animation based on movement direction
+	if direction != Vector2.ZERO:
+
+		if abs(direction.x) > abs(direction.y):
+			if direction.x > 0:
+				sprite.play("fly_right")
+			else:
+				sprite.play("fly_left")
+		else:
+			if direction.y > 0:
+				sprite.play("fly_down")
+			else:
+				sprite.play("fly_up")
+				
+	clamp_to_screen()
+	
+func clamp_to_screen():
+	var pos = global_position
+
+	pos.x = clamp(pos.x, margin.x, screen_size.x - margin.x)
+	pos.y = clamp(pos.y, margin.y, screen_size.y - margin.y)
+
+	global_position = pos
--- wind-turbine/scenes/turbine.gd
+++ wind-turbine/scenes/turbine.gd
@@ -3,8 +3,16 @@ extends Node2D
 @onready var blades = $MainBlades
 
 var spin_speed = 1.5
-var growth_speed = 0.2
+var time_alive = 0.0
 
 func _process(delta):
+	time_alive += delta
+
+	spin_speed = 1.5 + (time_alive * 0.05)
+
 	blades.rotation += spin_speed * delta
-	scale += Vector2.ONE * growth_speed * delta
+
+
+func _on_body_entered(body):
+	if body.is_in_group("player"):
+		get_tree().call_deferred("reload_current_scene")
